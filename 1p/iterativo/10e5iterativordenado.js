f519f52c45180ce75fa277c7452e36ad5fe0e50a
FlorencioOrozco <72531146+FlorencioOrozco@users.noreply.github.com>
2020-11-12 12:05:59 -0600

    Add files via upload

---
+++ 1p/iterativo/10e5iterativordenado.js
@@ -0,0 +1,46 @@
+//Orozco Valdez Florencio Miguel 3E
+
+let n = [];
+let x = 10; 
+let inversiones = 0;
+
+for(z = 0; z < x; z++) {
+    n.push(Math.round(Math.random()*Math.pow(10,5)))
+}
+console.log("Como viene por defecto :") 
+for(x = 0; x < n.length; x++)
+{
+   console.log( "Ubicación [",x,"] =",n[x] )
+   
+   inversiones++;
+}
+function funcionOrdenar ()
+{
+ for(i = 0; i < n.length; i++)
+   for(j = 0; j< n.length ; j++)
+   {
+      if(n[j]>n[j+1])
+      {
+          let a = n[j]        
+          n[j] = n[j + 1];    
+          n[j + 1] = a        
+      
+          inversiones++;
+        }
+    }
+ console.log("\nOrdenado : ")
+ for(i = 0; i < n.length; i++)
+{
+   console.log( "Ubicación [",i,"] =",n[i] )
+}
+}
+console.time()
+console.timeEnd();
+funcionOrdenar();
+console.log("inversiones necesarias:", inversiones)
+
+//El resultado final de el promedio fue de 45 inversiones.
+//tiempo promedio 0.635ms
+//¿Se puede mejorar el tiempo?
+//Se pueden realizar diferentes métodos al que yo realicé y también depende de la potencia de la computadora, 
+//de como se haga el proceso de las funciones y for.
